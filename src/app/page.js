ace1f8c56d550ace8734bdae9cb03cdc322a38d6
Ninad Vyas <115214615+NinadVyas@users.noreply.github.com>
2023-09-02 19:52:27 +0530

    added ui and basic preloader

---
+++ src/app/page.js
@@ -0,0 +1,53 @@
+'use client';
+import { useEffect,useState } from 'react';
+import styles from './page.module.css'
+import Intro from '../components/Intro';
+import Description from '../components/Description';
+// import detail from '../components/Xdetails';
+import { AnimatePresence } from 'framer-motion';
+import Preloader from '../components/Preloader';
+import Projects from '../components/Galaxy';
+
+export default function Home() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect( () => {
+    (
+      async () => {
+          const LocomotiveScroll = (await import('locomotive-scroll')).default
+          const locomotiveScroll = new LocomotiveScroll();
+
+          setTimeout( () => {
+            setIsLoading(false);
+            document.body.style.cursor = 'default'
+            window.scrollTo(0,0);
+          }, 2000)
+      }
+    )()
+  }, [])
+
+  useEffect( () => {
+    (
+      async () => {
+          const LocomotiveScroll = (await import('locomotive-scroll')).default
+          const locomotiveScroll = new LocomotiveScroll();
+      }
+    )()
+  }, [])
+
+  return (
+      <main className={styles.main}>
+<AnimatePresence mode='wait'>
+        {isLoading && <Preloader />}
+      </AnimatePresence>
+        <Intro />
+        
+        <Description />
+        <Projects />
+
+        {/* <detail /> */}
+
+      </main>
+  )
+}
---
+++ src/components/Description/index.jsx
@@ -0,0 +1,40 @@
+import React, { useLayoutEffect, useRef } from 'react'
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import gsap from 'gsap';
+import styles from './style.module.css';
+
+const phrases = ["Space, often referred to as the final frontier, is the immense, seemingly infinite expanse that exists beyond Earth's atmosphere.", " It encompasses all celestial objects, from stars and planets to asteroids and comets.", "Space is a vacuum, devoid of matter and atmosphere, which allows for the unique conditions and phenomena we observe in the universe.",]
+
+export default function Index() {
+
+  return (
+    <div className={styles.description} >
+        {
+            phrases.map( (phrase, index) => {
+                return <AnimatedText key={index}>{phrase}</AnimatedText>
+            })
+        }
+    </div>
+  )
+}
+
+function AnimatedText({children}) {
+    const text = useRef(null);
+
+    useLayoutEffect( () => {
+        gsap.registerPlugin(ScrollTrigger);
+        gsap.from(text.current, {
+            scrollTrigger: {
+                trigger: text.current,
+                scrub: true,
+                start: "0px bottom",
+                end: "bottom+=400px bottom",
+            },
+            opacity: 0,
+            left: "-200px",
+            ease: "power3.Out"
+        })
+    }, [])
+
+    return <p ref={text}>{children}</p>
+}
