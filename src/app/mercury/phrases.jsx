4efdcdaa9c521aaab0899f38493dfcc556b01be1
Ninad Vyas <115214615+NinadVyas@users.noreply.github.com>
2023-11-18 11:53:29 +0530

    made changes

---
+++ src/app/mercury/phrases.jsx
@@ -0,0 +1,5 @@
+'use client';
+
+export const phrases = [
+    "Mercury, the smallest and innermost planet in our solar system, presents a fascinating celestial entity within the vast expanse of the Milky Way galaxy.",
+];
