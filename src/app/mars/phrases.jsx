e632daa37cf2aca8245ace6d2a4c0e8c38c4cdbb
Ninad Vyas <115214615+NinadVyas@users.noreply.github.com>
2023-11-21 23:25:08 +0530

    made changes

---
+++ src/app/mars/phrases.jsx
@@ -0,0 +1,5 @@
+'use client';
+
+export const phrases = [
+    "Mars, often referred to as the 'Red Planet,' continues to captivate scientists and space enthusiasts as a potential candidate for future human exploration due to its similarities to Earth and the possibility of sustaining life in the past or potentially in the future.    ",
+];
