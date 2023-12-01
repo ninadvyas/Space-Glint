62012fb7c51c181ee8338c665cd4979536735288
Ninad Vyas <115214615+NinadVyas@users.noreply.github.com>
2023-12-01 22:18:58 +0530

    made changes

---
+++ src/components/About/index.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+export class index extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+      </div>
+    )
+  }
+}
+
+export default index
